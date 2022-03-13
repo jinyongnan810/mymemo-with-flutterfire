aa7c720c2a7f762b21b0e72cb331d680b2b4cae9
Kin Yuunan <yuunan.kin@gmail.com>
2022-03-13 11:27:05 +0900

    initial

---
+++ android/app/src/main/kotlin/com/example/mymemo_with_flutterfire/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mymemo_with_flutterfire
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+      ),
+      home: const MyHomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Kin's Page"),
+      ),
+      body: const Center(child: Text('empty')),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.edit),
+        onPressed: () {},
+      ),
+    );
+  }
+}
