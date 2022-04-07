7ce639d9fe715e78905af3f2c956aae3385d6288
Kin Yuunan <yuunan.kin@gmail.com>
2022-04-08 08:31:53 +0900

    edit content

---
+++ lib/components/memo-editor.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:mymemo_with_flutterfire/models/memo.dart';
+import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:mymemo_with_flutterfire/shared/markdown_extensions.dart';
+import 'package:mymemo_with_flutterfire/shared/split.dart';
+
+class MemoEditor extends StatefulWidget {
+  final Memo memo;
+
+  const MemoEditor({Key? key, required this.memo}) : super(key: key);
+
+  @override
+  State<MemoEditor> createState() => _MemoEditorState();
+}
+
+class _MemoEditorState extends State<MemoEditor> {
+  String _title = '';
+  String _content = '';
+  final TextEditingController _contentEditor = TextEditingController();
+  @override
+  void initState() {
+    _title = widget.memo.title;
+    _contentEditor.text = widget.memo.content;
+    _content = widget.memo.content;
+    _contentEditor.addListener(() {
+      setState(() {
+        _content = _contentEditor.text;
+      });
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _contentEditor.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      Center(child: Text(_title)),
+      Expanded(
+          child: Split(
+              axis: Axis.horizontal,
+              initialFirstFraction: 0.5,
+              firstChild: TextField(
+                keyboardType: TextInputType.multiline,
+                maxLines: null,
+                controller: _contentEditor,
+              ),
+              secondChild: Markdown(
+                extensionSet: MarkdownExtensionSet.githubWeb.value,
+                data: _content,
+              )))
+    ]);
+  }
+}
--- lib/pages/memo-detail.dart
+++ lib/pages/memo-detail.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:mymemo_with_flutterfire/components/memo-editor.dart';
 import 'package:mymemo_with_flutterfire/providers/memos.dart';
-import 'package:mymemo_with_flutterfire/shared/markdown_extensions.dart';
 import 'package:provider/provider.dart';
 
 class MemoDetailPage extends StatelessWidget {
@@ -13,15 +12,11 @@ class MemoDetailPage extends StatelessWidget {
     final id = ModalRoute.of(context)!.settings.arguments as String;
     final memo = Provider.of<Memos>(context).getItemById(id);
     return Scaffold(
-      appBar: AppBar(title: Text(memo?.title ?? 'Not found')),
-      body: Column(children: [
-        Center(child: Text(memo?.title ?? 'Not found')),
-        Expanded(
-            child: Markdown(
-          extensionSet: MarkdownExtensionSet.githubWeb.value,
-          data: memo?.content ?? '',
-        ))
-      ]),
-    );
+        appBar: AppBar(title: Text(memo?.title ?? 'Not found')),
+        body: memo == null
+            ? const Center(
+                child: Text('Memo not found.'),
+              )
+            : MemoEditor(memo: memo));
   }
 }
