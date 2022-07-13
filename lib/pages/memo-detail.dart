d8b7e15285e4e251f39441307761153dbf6ef1aa
Kin Yuunan <yuunan.kin@gmail.com>
2022-07-14 07:11:34 +0900

    replace conditions with Visiblility

--- lib/pages/memo-detail.dart
+++ lib/pages/memo-detail.dart
@@ -58,81 +58,80 @@ class _MemoDetailPageState extends State<MemoDetailPage> {
                 end: Alignment.bottomRight,
                 colors: [Colors.purple, Colors.orange])),
         child: Scaffold(
-          extendBodyBehindAppBar: true,
-          backgroundColor: Colors.transparent,
-          appBar: AppBar(
-            title: Text(_loading ? 'Loading...' : memo?.title ?? 'Not found'),
+            extendBodyBehindAppBar: true,
             backgroundColor: Colors.transparent,
-            elevation: 0,
-            // leading: Builder(
-            //   builder: ((context) => IconButton(
-            //       onPressed: () {
-            //         Navigator.of(context).pushReplacementNamed('/');
-            //       },
-            //       icon: const Icon(Icons.home))),
-            // ),
-          ),
-          body: _loading
-              ? const Loading()
-              : memo == null
-                  ? const Center(
-                      child: Text('Memo not found.'),
-                    )
-                  : _editing
-                      ? MemoEditor(memo: memo!)
-                      : MemoRendered(content: memo!.content),
-          floatingActionButton: (memo == null ||
-                  !auth.signedIn ||
-                  memo!.userId != auth.userId)
-              ? null
-              : Container(
-                  decoration: BoxDecoration(
-                      gradient: const LinearGradient(
-                          begin: Alignment.topLeft,
-                          end: Alignment.bottomRight,
-                          colors: [Colors.purple, Colors.orange]),
-                      borderRadius: BorderRadius.circular(30)),
-                  child: FloatingActionButton(
-                    backgroundColor: Colors.transparent,
-                    foregroundColor: Colors.white,
-                    elevation: 0,
-                    onPressed: () async {
-                      if (_editing) {
-                        final isNewMemo = memo!.id == null;
-                        try {
-                          await memo!.save();
-                          if (isNewMemo) {
-                            Provider.of<Memos>(context, listen: false)
-                                .addItem(memo!);
-                          } else {
-                            Provider.of<Memos>(context, listen: false).notify();
-                          }
-                        } catch (e) {
-                          print(Text(e.toString()));
-                          ScaffoldMessenger.of(context).showSnackBar(
-                              const SnackBar(
-                                  content: Text('Error saving memo.')));
-                          return;
-                        }
-                        ScaffoldMessenger.of(context).showSnackBar(
-                            const SnackBar(content: Text('Memo saved.')));
-                        setState(() {
-                          _editing = false;
-                        });
+            appBar: AppBar(
+              title: Text(_loading ? 'Loading...' : memo?.title ?? 'Not found'),
+              backgroundColor: Colors.transparent,
+              elevation: 0,
+              // leading: Builder(
+              //   builder: ((context) => IconButton(
+              //       onPressed: () {
+              //         Navigator.of(context).pushReplacementNamed('/');
+              //       },
+              //       icon: const Icon(Icons.home))),
+              // ),
+            ),
+            body: _loading
+                ? const Loading()
+                : memo == null
+                    ? const Center(
+                        child: Text('Memo not found.'),
+                      )
+                    : _editing
+                        ? MemoEditor(memo: memo!)
+                        : MemoRendered(content: memo!.content),
+            floatingActionButton: Visibility(
+              visible:
+                  memo != null && auth.signedIn && memo!.userId == auth.userId,
+              child: Container(
+                decoration: BoxDecoration(
+                    gradient: const LinearGradient(
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight,
+                        colors: [Colors.purple, Colors.orange]),
+                    borderRadius: BorderRadius.circular(30)),
+                child: FloatingActionButton(
+                  backgroundColor: Colors.transparent,
+                  foregroundColor: Colors.white,
+                  elevation: 0,
+                  onPressed: () async {
+                    if (_editing) {
+                      final isNewMemo = memo!.id == null;
+                      try {
+                        await memo!.save();
                         if (isNewMemo) {
-                          GoRouter.of(context).go('/memos/${memo!.id}');
+                          Provider.of<Memos>(context, listen: false)
+                              .addItem(memo!);
+                        } else {
+                          Provider.of<Memos>(context, listen: false).notify();
                         }
-                      } else {
-                        setState(() {
-                          _editing = true;
-                        });
+                      } catch (e) {
+                        print(Text(e.toString()));
+                        ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                                content: Text('Error saving memo.')));
+                        return;
                       }
-                    },
-                    child: _editing
-                        ? const Icon(Icons.save)
-                        : const Icon(Icons.edit),
-                  ),
+                      ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(content: Text('Memo saved.')));
+                      setState(() {
+                        _editing = false;
+                      });
+                      if (isNewMemo) {
+                        GoRouter.of(context).go('/memos/${memo!.id}');
+                      }
+                    } else {
+                      setState(() {
+                        _editing = true;
+                      });
+                    }
+                  },
+                  child: _editing
+                      ? const Icon(Icons.save)
+                      : const Icon(Icons.edit),
                 ),
-        ));
+              ),
+            )));
   }
 }
--- lib/pages/memo-list.dart
+++ lib/pages/memo-list.dart
@@ -13,94 +13,94 @@ class MemoListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: const BoxDecoration(
-          gradient: LinearGradient(
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-              colors: [Colors.purple, Colors.orange])),
-      child: Scaffold(
-          extendBodyBehindAppBar: true,
-          backgroundColor: Colors.transparent,
-          appBar: AppBar(
-            title: const Text("Kin's Page"),
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [Colors.purple, Colors.orange])),
+        child: Scaffold(
+            extendBodyBehindAppBar: true,
             backgroundColor: Colors.transparent,
-            elevation: 0,
-            actions: [
-              IconButton(
-                  onPressed: () {
-                    showSearch(
-                        context: context, delegate: MemoListSearchDelegate());
-                  },
-                  icon: const Icon(Icons.search))
-            ],
-            leading:
-                // currently only supports web
-                kIsWeb
-                    ? Consumer<Auth>(
-                        builder: (ctx, auth, _) => auth.signedIn
-                            ? Tooltip(
-                                child: Padding(
-                                    padding: const EdgeInsets.only(left: 10),
-                                    child: PopupMenuButton(
-                                      tooltip: auth.myProfile.email,
-                                      position: PopupMenuPosition.under,
-                                      onSelected: (value) async {
-                                        if (value == 'signOut') {
-                                          await auth.signOut();
-                                        }
-                                      },
-                                      itemBuilder: (ctx) => [
-                                        const PopupMenuItem(
-                                          child: Text('Sign out'),
-                                          value: 'signOut',
+            appBar: AppBar(
+              title: const Text("Kin's Page"),
+              backgroundColor: Colors.transparent,
+              elevation: 0,
+              actions: [
+                IconButton(
+                    onPressed: () {
+                      showSearch(
+                          context: context, delegate: MemoListSearchDelegate());
+                    },
+                    icon: const Icon(Icons.search))
+              ],
+              leading:
+                  // currently only supports web
+                  kIsWeb
+                      ? Consumer<Auth>(
+                          builder: (ctx, auth, _) => auth.signedIn
+                              ? Tooltip(
+                                  child: Padding(
+                                      padding: const EdgeInsets.only(left: 10),
+                                      child: PopupMenuButton(
+                                        tooltip: auth.myProfile.email,
+                                        position: PopupMenuPosition.under,
+                                        onSelected: (value) async {
+                                          if (value == 'signOut') {
+                                            await auth.signOut();
+                                          }
+                                        },
+                                        itemBuilder: (ctx) => [
+                                          const PopupMenuItem(
+                                            child: Text('Sign out'),
+                                            value: 'signOut',
+                                          ),
+                                        ],
+                                        child: CircleAvatar(
+                                          radius: 20,
+                                          // in Appbar, ragular background image won't fit
+                                          // this way worked
+                                          child: ClipOval(
+                                              child: Image.network(
+                                            auth.myProfile.photoUrl,
+                                          )),
                                         ),
-                                      ],
-                                      child: CircleAvatar(
-                                        radius: 20,
-                                        // in Appbar, ragular background image won't fit
-                                        // this way worked
-                                        child: ClipOval(
-                                            child: Image.network(
-                                          auth.myProfile.photoUrl,
-                                        )),
-                                      ),
-                                    )),
-                                message:
-                                    'Signed in with ${auth.myProfile.email}. Click to sign out.',
-                              )
-                            : Tooltip(
-                                child: IconButton(
-                                    onPressed: () async {
-                                      await auth.signIn();
-                                    },
-                                    icon: const Icon(Icons.person)),
-                                message: 'Sign in',
-                              ))
-                    : Container(),
-          ),
-          body: const MemoList(),
-          floatingActionButton: kIsWeb
-              ? Consumer<Auth>(
-                  builder: (ctx, auth, _) => auth.signedIn
-                      ? Container(
-                          decoration: BoxDecoration(
-                              gradient: const LinearGradient(
-                                  begin: Alignment.topLeft,
-                                  end: Alignment.bottomRight,
-                                  colors: [Colors.purple, Colors.orange]),
-                              borderRadius: BorderRadius.circular(30)),
-                          child: FloatingActionButton(
-                            child: const Icon(Icons.add),
-                            backgroundColor: Colors.transparent,
-                            foregroundColor: Colors.white,
-                            elevation: 0,
-                            onPressed: () {
-                              GoRouter.of(context).go('/memos/new');
-                            },
-                          ),
-                        )
-                      : Container())
-              : Container()),
-    );
+                                      )),
+                                  message:
+                                      'Signed in with ${auth.myProfile.email}. Click to sign out.',
+                                )
+                              : Tooltip(
+                                  child: IconButton(
+                                      onPressed: () async {
+                                        await auth.signIn();
+                                      },
+                                      icon: const Icon(Icons.person)),
+                                  message: 'Sign in',
+                                ))
+                      : Container(),
+            ),
+            body: const MemoList(),
+            floatingActionButton: Visibility(
+              visible: kIsWeb,
+              child: Consumer<Auth>(
+                  builder: (ctx, auth, _) => Visibility(
+                      visible: auth.signedIn,
+                      child: Container(
+                        decoration: BoxDecoration(
+                            gradient: const LinearGradient(
+                                begin: Alignment.topLeft,
+                                end: Alignment.bottomRight,
+                                colors: [Colors.purple, Colors.orange]),
+                            borderRadius: BorderRadius.circular(30)),
+                        child: FloatingActionButton(
+                          child: const Icon(Icons.add),
+                          backgroundColor: Colors.transparent,
+                          foregroundColor: Colors.white,
+                          elevation: 0,
+                          onPressed: () {
+                            GoRouter.of(context).go('/memos/new');
+                          },
+                        ),
+                      ))),
+            )));
   }
 }
