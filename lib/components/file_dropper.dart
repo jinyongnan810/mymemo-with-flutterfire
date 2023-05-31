0a78ee31c3f4cc921af5b975b5dfc9c989e81e41
Kin Yuunan <yuunan.kin@gmail.com>
2023-06-01 08:35:07 +0900

    upload video day2

---
+++ lib/components/file_dropper.dart
@@ -0,0 +1,50 @@
+import 'dart:io';
+
+import 'package:desktop_drop/desktop_drop.dart';
+import 'package:dotted_border/dotted_border.dart';
+import 'package:flutter/material.dart';
+
+class FileDropper extends StatelessWidget {
+  const FileDropper({super.key, required this.onDroppedFiles});
+  final void Function(List<File>) onDroppedFiles;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: DottedBorder(
+        dashPattern: const [20, 8],
+        color: Colors.white,
+        strokeWidth: 5,
+        strokeCap: StrokeCap.round,
+        borderType: BorderType.RRect,
+        radius: const Radius.circular(20),
+        child: DropTarget(
+          onDragDone: (details) {
+            print('onDragDone: ${details.files}');
+            final files = details.files.map((e) => File(e.path)).toList();
+            onDroppedFiles(files);
+          },
+          onDragEntered: (details) {},
+          onDragExited: (details) {},
+          child: const SizedBox(
+            width: 300,
+            child: Align(
+              alignment: Alignment.center,
+              child: Padding(
+                padding: EdgeInsets.all(12.0),
+                child: Text(
+                  'Drag and drop video&thumbnail here',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/components/video_uploader.dart
+++ lib/components/video_uploader.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:mymemo_with_flutterfire/components/file_dropper.dart';
 
 class VideoUploader extends StatefulWidget {
   const VideoUploader({super.key});
@@ -17,7 +18,14 @@ class _VideoUploaderState extends State<VideoUploader> {
   Widget build(BuildContext context) {
     return AlertDialog(
       title: const Text('Upload Video'),
-      content: const Text('Not implemented yet'),
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          FileDropper(onDroppedFiles: onDroppedFiles),
+          Text('Video: ${_video?.path ?? 'None'}'),
+          Text('Thumbnail: ${_thumbnail?.path ?? 'None'}'),
+        ],
+      ),
       actions: [
         TextButton(
           onPressed: () => Navigator.pop(context),
@@ -29,5 +37,20 @@ class _VideoUploaderState extends State<VideoUploader> {
         ),
       ],
     );
+  }
+
+  void onDroppedFiles(List<File> files) {
+    // TODO: onDroppedFiles: [File: 'blob:http://localhost:1234/1e125af7-43ee-4d20-8199-50ddcb526e88', File: 'blob:http://localhost:1234/ec57b7a5-f60f-4875-93e3-92dc8d086544']
+    debugPrint('onDroppedFiles: $files');
+    final videos =
+        files.where((file) => ['.mp4', '.mov'].any(file.path.endsWith));
+    final pictures =
+        files.where((file) => ['.png', '.jpg'].any(file.path.endsWith));
+    debugPrint(videos.toString());
+    debugPrint(pictures.toString());
+    setState(() {
+      _video = videos.isNotEmpty ? videos.first : null;
+      _thumbnail = pictures.isNotEmpty ? pictures.first : null;
+    });
   }
 }
